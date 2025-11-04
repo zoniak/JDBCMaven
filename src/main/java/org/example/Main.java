446a83abfa30f7ff1c6bde9264675579021b077e
Zoniak <zoniakzoak@gmail.com>
2025-11-04 09:03:01 +0100

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.sql.*;
+import javax.sql.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        IO.println(String.format("Hello and welcome!"));
+        DriverManager.drivers().forEach(driver -> System.out.println(driver.toString()));
+
+
+    }
+}
